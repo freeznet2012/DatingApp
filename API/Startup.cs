59eba4555d12e34fc3a36accde14be43bdea2ff8
Praveen benny <praveenbenny007@gmail.com>
2021-09-02 00:57:16 +0530

    end of section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -19,9 +19,11 @@ namespace API
     public class Startup
     {
         private readonly IConfiguration _config;
+
         public Startup(IConfiguration config)
         {
             _config = config;
+
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -29,9 +31,11 @@ namespace API
         {
 
             services.AddControllers();
-            services.AddDbContext<DataContext>((options) => {
+            services.AddDbContext<DataContext>((options) =>
+            {
                 options.UseSqlite(_config.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors();
             // services.AddSwaggerGen(c =>
             // {
             //     c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
@@ -51,6 +55,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
             app.UseAuthorization();
 
